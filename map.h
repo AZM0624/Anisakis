71f0cf4e4adef6db84a28d13729ab8182986f701
AZM0624 <kaisei20051018@gmail.com>
2026-02-09 13:27:55 +0900

    Fix comment formatting in map.h

--- map.h
+++ map.h
@@ -3,7 +3,7 @@
 
 #define MAP_WIDTH 24
 #define MAP_HEIGHT 24
-#define TILE_SIZE 64 // 1タイルのピクセルサイズ（2Dマップ上での話）
+#define TILE_SIZE 64 // 1タイルのピクセルサイズ
 
 // 0 = 床, 1以上 = 壁
 extern int worldMap[MAP_WIDTH][MAP_HEIGHT];
